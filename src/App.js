5ab675247981ae97d3631c9a78a174b0c87f41ac
ThanhPhongsof <ttphongletter@gmail.com>
2022-06-01 14:28:49 +0700

    coding UI

--- src/App.js
+++ src/App.js
@@ -7,19 +7,7 @@ const { Title } = Typography;
 
 function App() {
   return (
-    <div
-      style={{
-        width: 500,
-        margin: "0 auto",
-        display: "flex",
-        flexDirection: "column",
-        backgroundColor: "white",
-        padding: 20,
-        boxShadow: "0 0 10px 4px #bfbfbf",
-        borderRadius: 5,
-        height: "90vh",
-      }}
-    >
+    <div className="app">
       <Title style={{ textAlign: "center" }}>TODO APP with REDUX</Title>
       <Filters />
       <Divider />
